b7c0f679550372d3f8634e56de6f209abda07f94
Victor Boyd <115438604+LeKnee@users.noreply.github.com>
2023-09-18 15:08:47 -0500

    Add files via upload
    added print for unit elasticity

--- midpoint.py
+++ midpoint.py
@@ -1,10 +1,10 @@
 import math
 
 # Input values
-price1 = 50
-price2 = 175
-quantity1 = 56
-quantity2 = 36
+price1 = 175
+price2 = 225
+quantity1 = 36
+quantity2 = 28
 
 # Calculating the midpoint of price and quantity
 midpointprice = (price1 + price2) / 2
@@ -26,7 +26,9 @@ priceelasticity = quantitypercentagechange / pricepercentagechange
 # Print Price elasticity
 print(priceelasticity)
 
-if abs(priceelasticity) >= 1:
+if abs(priceelasticity) > 1:
     print ("Price is elastic")
+elif abs(priceelasticity) == 1:
+    print ("Price is unit elastic")
 else:
     print ("Price is not elastic")
